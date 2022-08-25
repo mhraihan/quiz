2c45ca6a94a8bf7f0e5a02a6bc3518c52a189556
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2022-08-26 04:53:52 +0600

    vite change

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            ziggy: "/vendor/tightenco/ziggy/src/js",
-            "ziggy-vue": "/vendor/tightenco/ziggy/src/js/vue",
-        },
-    },
     plugins: [
         laravel({
             input: "resources/js/app.js",
